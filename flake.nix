49fc07088878ac307b7ef668a1c62f21fdb44691
qwerty <temp@qwerty.qq>
2025-08-09 23:48:04 +0300

    refactor

--- flake.nix
+++ flake.nix
@@ -11,6 +11,7 @@
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs:
     let
+      # --- Карта хостов, как и была ---
       nix-hosts = {
         "qwerty" = {
           username = "qwerty";
@@ -19,22 +20,29 @@
         };
       };
 
-      makeSystem = { hostname, config }:
+      # --- ИСПРАВЛЕННАЯ функция-конструктор ---
+      # Она принимает имя хоста и его конфигурацию как два отдельных аргумента.
+      makeSystem = hostname: config:
         nixpkgs.lib.nixosSystem {
           system = config.system;
-          specialArgs = { inherit inputs; } // config; 
+          specialArgs = { inherit inputs hostname; } // config; # Передаем hostname и всю конфигурацию хоста
 
           modules = [
+            # Системные модули
             ./system/configuration.nix
 
+            # Модуль Home Manager
             home-manager.nixosModules.home-manager
             {
+              # Конфигурация Home Manager для конкретного пользователя
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.backupFileExtension = "hm-backup";
 
-              home-manager.extraSpecialArgs = { inherit inputs; } // config;
+              # Передаем аргументы в модули Home Manager
+              home-manager.extraSpecialArgs = { inherit inputs hostname; } // config;
 
+              # Активируем конфигурацию для пользователя
               home-manager.users."${config.username}" = {
                 imports = [ ./home/home.nix ];
               };

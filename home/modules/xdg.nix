b3cf0ff7f7f214c49c024c9857d3a4039199b12b
qwerty <temp@qwerty.qq>
2025-09-26 18:56:11 +0300

    update xdg

--- home/modules/xdg.nix
+++ home/modules/xdg.nix
@@ -1,9 +1,9 @@
+{ config, ... }:
+
 {
   xdg.userDirs = {
     enable = true;
-
     createDirectories = true;
-
     desktop = "$HOME/Desktop";
     documents = "$HOME/Documents";
     download = "$HOME/Downloads";
@@ -14,20 +14,45 @@
     videos = "$HOME/Videos";
   };
 
-  xdg.mimeApps = {
-    enable = true;
-    defaultApplications = {
-      "image/png" = [ "org.gnome.Loupe.desktop" ];
-      "image/jpeg" = [ "org.gnome.Loupe.desktop" ];
-      "image/gif" = [ "org.gnome.Loupe.desktop" ];
-      "image/webp" = [ "org.gnome.Loupe.desktop" ];
-      "image/svg+xml" = [ "org.gnome.Loupe.desktop" ];
+  xdg.mimeApps =
+    let
+      zenDesktopFile = config.programs.zen-browser.package.meta.desktopFileName;
 
-      "x-scheme-handler/http" = [ "zen-beta.desktop" ];
-      "x-scheme-handler/https" = [ "zen-beta.desktop" ];
-      "x-scheme-handler/about" = [ "zen-beta.desktop" ];
-      "x-scheme-handler/unknown" = [ "zen-beta.desktop" ];
-      "text/html" = [ "zen-beta.desktop" ];
+      browserMimeTypes = [
+        "application/x-extension-shtml"
+        "application/x-extension-xhtml"
+        "application/x-extension-html"
+        "application/x-extension-xht"
+        "application/x-extension-htm"
+        "x-scheme-handler/unknown"
+        "x-scheme-handler/mailto"
+        "x-scheme-handler/chrome"
+        "x-scheme-handler/about"
+        "x-scheme-handler/https"
+        "x-scheme-handler/http"
+        "application/xhtml+xml"
+        "text/html"
+      ];
+
+      browserApps = builtins.listToAttrs (map (name: {
+        inherit name;
+        value = [ zenDesktopFile ];
+      }) browserMimeTypes);
+
+      imageApps = {
+        "image/png" = [ "org.gnome.Loupe.desktop" ];
+        "image/jpeg" = [ "org.gnome.Loupe.desktop" ];
+        "image/gif" = [ "org.gnome.Loupe.desktop" ];
+        "image/webp" = [ "org.gnome.Loupe.desktop" ];
+        "image/svg+xml" = [ "org.gnome.Loupe.desktop" ];
+      };
+
+      allDefaultApps = imageApps // browserApps;
+
+    in
+    {
+      enable = true;
+      defaultApplications = allDefaultApps;
+      associations.added = allDefaultApps;
     };
-  };
 }
